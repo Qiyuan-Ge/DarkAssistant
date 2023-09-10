f7c9ca1b0d96bc141bb2ec4df9c2390ce050a255
Qiyuan-Ge <53368178+Qiyuan-Ge@users.noreply.github.com>
2023-09-10 11:16:24 +0800

    Update finish.py

--- assistant/tools/finish.py
+++ assistant/tools/finish.py
@@ -3,5 +3,5 @@ from langchain.agents import Tool
 finish_tool = Tool(
     name="Final Response",
     func=lambda x: None,
-    description='response to user if you know the final answer or complete all tasks, args: <markdown format output>',
+    description='response to user if you know the final answer or complete all tasks, args: <your final answer in markdown format>',
     )
